d7ceb052cab8a7ef4b97a217114a1ae1b2ac9835
Rob Sewell <mrrobsewell@outlook.com>
2024-07-07 19:40:40 +0000

    so we cn dab like Usain

--- dab/demo.ps1
+++ dab/demo.ps1
@@ -19,12 +19,40 @@ GO
 
 cd dab
 
-dab init --database-type "mssql" --host-mode "Development" --connection-string "Server=localhost,1433;User Id=mrrob;Database=AdventureWorksLT;Password=1dxz1q!Y$H@71rZ4m%X8D7B7&JE6#McKq!!HQy%6hG*$l&L0%3;TrustServerCertificate=True;Encrypt=True;"
+# lets create the data api builder
+
+dab init --database-type "mssql" --host-mode "Development" --connection-string "Server=localhost,15444;User Id=dab;Database=AdventureWorksLT;Password=PASSword01!;TrustServerCertificate=True;Encrypt=True;"
+
+# look at the new config file
+code /workspaces/SQLDevDemos/dab/dab-config.json
+
+# Now we can start the data api builder :-)
 
 dab start
 
+# and take a look at it
+
 http://localhost:5000
 http://127.0.0.1:8080/
+
+http://localhost:5000/api/Product/ProductID/757
+http://127.0.0.1:8080/api/Product/ProductID/757
+
+# Hmm there are no entitities created.
+
+# We could
+
+# add them all ourselves individually
+
+# but we have an existing database and maybe a lot of entities
+
+# lets use Davide's magic T-SQL script of awesome!
+
+code /workspaces/SQLDevDemos/dab/sql-to-dab-config.sql
+
+# add the results to the correct place in the config file and restart dab
+
+# Now look !!
 
 http://localhost:5000/api/Product/ProductID/757
 http://127.0.0.1:8080/api/Product/ProductID/757
